4ac08b33574448aedcf9681380f228ffd611b0b9
jparba <geminix2793@gmail.com>
2020-04-30 07:18:27 +0800

    fix link

--- src/components/AllListCovid.vue
+++ src/components/AllListCovid.vue
@@ -83,7 +83,7 @@
 		</div>
 		<ul v-if="coronaLists != '' && !loading" class="row country_affected">
 			<li class="col-md-4 col-sm-6 mb-4" v-for="(coronaList, index) in coronaLists" :key="index">
-				<router-link :to="{ path: `/country/${coronaList.country}` }">
+				<router-link :to="{ path: `/covid-19/country/${coronaList.country}` }">
 				<!-- <a href=""> -->
 					<div class="card">
 						<div class="card-body">
