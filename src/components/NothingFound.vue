bcc7be015a276301f2ed603b1438c1fde28b8fea
jparba <geminix2793@gmail.com>
2020-04-30 06:57:07 +0800

    add style

--- src/components/NothingFound.vue
+++ src/components/NothingFound.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="d-flex flex-column align-items-center">
+	<div class="d-flex flex-column align-items-center container">
 		<p class="mt-5 text-center">Sorry the page you requested is not found.</p>
-		<router-link to="/covid-19" class="btn btn-success">Back to Covid-19 bulletin</router-link>
+		<router-link to="/covid-19" class="btn btn-success mb-2">Back to Covid-19 bulletin</router-link>
 		<img alt="Vue logo" src="../assets/404.png">
 	</div>
 </template>
